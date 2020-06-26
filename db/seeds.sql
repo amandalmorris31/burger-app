f93aee2f93c309373f108a01add604b0653b50cc
Amanda Morris <amandahenry2@comcast.net>
2020-06-26 15:18:50 -0600

    connecting to JAWS DB

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+USE burger_db;
+INSERT INTO burgers (burger_name) VALUES ('Fuego Jalapeno Tempeh');
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Turkey');
+INSERT INTO burgers (burger_name) VALUES ('Quinoa Veggie');
